044428baac002cbe25476c4f46b763a20cec4b0b
Nithin.A <nithin.akuthota@rm1codershub.com>
2025-10-15 16:05:17 +0530

    Implemented employee grid

--- src/Routes/secureRoutes/SecureRoutes.tsx
+++ src/Routes/secureRoutes/SecureRoutes.tsx
@@ -3,12 +3,14 @@ import Dashboard from "../../pages/dashboard/Dashboard";
 import LayoutComponent from "../../components/Layout/Layout";
 import Settings from "../../pages/settings/settings";
 import Register from "../../pages/register/Register";
+import Employees from "../../pages/employees/Employees";
 
 export const SecureRoutes = () => {
     
     return <Routes>
         <Route path="/" element={<LayoutComponent />}>
             <Route path="dashboard" element={<Dashboard />} />
+            <Route path="employees" element={<Employees />} />
             
             <Route path="settings" element={<Settings />} />
         </Route>
