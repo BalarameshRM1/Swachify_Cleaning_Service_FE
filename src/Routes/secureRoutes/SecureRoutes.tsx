a7c4210ddb9561effda8e5df3fa1f8fa7c08ced6
tejasriram944 <tejasriram@rm1codershub.com>
2025-10-16 15:10:37 +0530

    resolved merge conflicts

--- src/Routes/secureRoutes/SecureRoutes.tsx
+++ src/Routes/secureRoutes/SecureRoutes.tsx
@@ -3,6 +3,8 @@ import Dashboard from "../../pages/dashboard/Dashboard";
 import LayoutComponent from "../../components/Layout/Layout";
 import Settings from "../../pages/settings/settings";
 import Services from "../../pages/services/Services";
+import Employees from "../../pages/employees/Employees";
+
 
 export const SecureRoutes = () => {
     
@@ -10,6 +12,7 @@ export const SecureRoutes = () => {
         <Route path="/" element={<LayoutComponent />}>
             <Route path="dashboard" element={<Dashboard />} />   
             <Route path="settings" element={<Settings />} />
+            <Route path="employees" element={<Employees />} />
             <Route path="services" element={<Services />} />
         </Route>
     </Routes>
