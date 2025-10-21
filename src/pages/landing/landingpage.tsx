8ce4a10f0d12e87c13f3f9200c17061ae6508245
Nithin.A <nithin.akuthota@rm1codershub.com>
2025-10-22 04:51:12 +0530

    implemented tickets scroll bar

--- src/pages/landing/landingpage.tsx
+++ src/pages/landing/landingpage.tsx
@@ -1193,7 +1193,7 @@ const handleLogin = async (values: any) => {
             <Form.Item
               label="Password"
               name="password"
-              normalize={(value) => value.trim()}
+              
               rules={[{ required: true, message: 'Please enter your password' }]}
             >
               <Input.Password size="large" placeholder="Enter password" />
