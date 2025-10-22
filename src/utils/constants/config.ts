312919bf865bb455496c5ad8f56b05e3615d7b63
HarishyadavRM1 <harishyadav.pilli@rm1codershub.com>
2025-10-22 20:29:19 +0530

    added changes

--- src/utils/constants/config.ts
+++ src/utils/constants/config.ts
@@ -1,5 +1,5 @@
 // export const baseUrl = import.meta.env.VITE_BASE_URL;
-export const baseUrl = "https://swachifyfe-hpg2dafuhudagra7.centralindia-01.azurewebsites.net/api";
+export const baseUrl = "https://swachifydemo-bfh4c3azdbd7bpdt.centralindia-01.azurewebsites.net/api";
 export const postsEndpoint = '/posts';
 export const usersEndpoint = '/users';
 export const commentsEndpoint = '/comments';
