f7c72f12b31f0e5590f9493862bef089140934f7
Tejesh Gudla <tejesharyagudla@gmail.com>
2025-10-31 18:48:06 +0530

    Tickets Summary added

--- src/pages/tickets/Tickets.tsx
+++ src/pages/tickets/Tickets.tsx
@@ -357,7 +357,8 @@ const Tickets: React.FC = () => {
                         {ticket?.status.charAt(0)?.toUpperCase() + ticket?.status.slice(1)}
                       </Text>{" "}
                       <Text style={{ marginLeft: 8, color: "#374151" }}>
-                        Ticket #{ticket?.id?.toString()?.slice(-6)}
+                        {/* Ticket #{ticket?.id?.toString()?.slice(-6)} */}
+                          {ticket.services.map((s:any)=>`${s.department_name} - ${s.service_name}`).join(", ")}
                       </Text>
                     </Col>
                     {ticket?.status=== "Completed" && (
