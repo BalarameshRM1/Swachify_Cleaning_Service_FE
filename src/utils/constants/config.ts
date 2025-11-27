bccd3e025f4cc12aaa5e980e1409fe88b58f93ec
Dudekula Khasim <khasim.dudekula@rm1codershub.com>
2025-11-27 12:02:15 +0530

    changes added

--- src/utils/constants/config.ts
+++ src/utils/constants/config.ts
@@ -1,5 +1,5 @@
 // export const baseUrl = import.meta.env.VITE_BASE_URL;
-export const baseUrl = "https://swachifyapi-fpcub9f8dcgjbzcq.centralindia-01.azurewebsites.net/api";
+export const baseUrl = "http://43.243.79.182/api";
 export const postsEndpoint = '/posts';
 export const usersEndpoint = '/users';
 export const commentsEndpoint = '/comments';
