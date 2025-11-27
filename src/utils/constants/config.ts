2b6f157ce55b1510ac087bda158b52134377d3a3
Dudekula Khasim <khasim.dudekula@rm1codershub.com>
2025-11-27 12:43:00 +0530

    config file updated

--- src/utils/constants/config.ts
+++ src/utils/constants/config.ts
@@ -1,5 +1,5 @@
 // export const baseUrl = import.meta.env.VITE_BASE_URL;
-export const baseUrl = "http://43.243.79.182/api";
+export const baseUrl = "http://clean.swachify.com/api";
 export const postsEndpoint = '/posts';
 export const usersEndpoint = '/users';
 export const commentsEndpoint = '/comments';
